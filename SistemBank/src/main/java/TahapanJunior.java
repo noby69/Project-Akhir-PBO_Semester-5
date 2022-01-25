8e954add673ca6cc14090882ca1f941523a140bc
TokyoReject69 <59589915+noby69@users.noreply.github.com>
2022-01-25 19:11:21 +0700

    Update TahapanJunior.java

--- SistemBank/src/main/java/TahapanJunior.java
+++ SistemBank/src/main/java/TahapanJunior.java
@@ -2,8 +2,7 @@ public class TahapanJunior extends Rekening{
     private static String tipeRekening = "Tahapan Junior";
     
     TahapanJunior(double jumlahDeposit){
-        this.setSaldo(jumlahDeposit);
-        this.setBunga(0.02);   
+        this.setSaldo(jumlahDeposit);  
     }
     @Override
     public String toString(){
